717b01469e581040e97733072da7621d813e0f3b
Dhanusha R <dhanusharamesh@gmail.com>
2026-06-02 17:32:41 +0000

    Day2

---
+++ Linux/DAY2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Shows disk space usage of file systems in a human-readable format : ps"
+ps
+echo "Shows disk space usage of file systems in a human-readable format : df -h"
+df -h
+echo "Displays RAM and swap memory usage in MB. (Linux only) : free -m"
+free -m
+echo "Lists previously executed commands in the terminal : history"
+history
+echo "Shows disk space usage of file systems in a human-readable format : top"
+top
